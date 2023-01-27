c0e3bccf4d1a19fce067eb3e2a5e0f5f5ad6fd06
Gustaf Jäderberg <gustaf@jaderb.se>
2023-01-27 09:44:38 +0100

    Change name

---
+++ Project/client/Api.js
@@ -0,0 +1,82 @@
+
+class Api {
+  
+  url = '';
+
+  
+  constructor(url) {
+    
+    this.url = url;
+  }
+
+  
+  create(data) {
+    
+    const JSONData = JSON.stringify(data);
+    
+    console.log(`Sending ${JSONData} to ${this.url}`);
+
+    const request = new Request(this.url, {
+      method: 'POST',
+      body: JSONData,
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+
+    
+    return (
+      
+      fetch(request)
+        
+        .then((result) => result.json())
+        
+        .then((data) => data)
+        
+        .catch((err) => console.log(err))
+    );
+  }
+
+  
+  getAll() {
+    
+    return fetch(this.url)
+      .then((result) => result.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  }
+
+  
+  remove(id) {
+    
+    console.log(`Removing task with id ${id}`);
+
+    
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE'
+    })
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  }
+
+  update(id, check) {
+
+    console.log(`Updating task with id ${id}`);
+
+    return fetch(this.url, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        id: id,
+        completed: check,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+
+    })
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  }
+
+  
+}
